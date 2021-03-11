8ecbadde886b303c17cfd6780ed8faad5ea0e532
yohei95 <yuhi0002@gmail.com>
2021-03-11 09:29:07 +0900

    パスワードバリデーションエラー文修正

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -57,7 +57,7 @@ require 'rails_helper'
         @user.password = '000000'
         @user.password_confirmation = '000000'
         @user.valid?
-        expect(@user.errors.full_messages).to include("Password には英字と数字の両方を含めて設定してください")
+        expect(@user.errors.full_messages).to include("Password is invalid")
       end
       it 'passwordが存在してもpassword_confirmationが空では登録できない' do
         @user.password_confirmation = ''
