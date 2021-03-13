343a147ea7ae83440f1329f0aa09f81b383300d5
yohei95 <yuhi0002@gmail.com>
2021-03-13 16:44:41 +0900

    商品編集機能修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create, :edit, :update]
-  before_action :move_to_index, only: :edit
+  before_action :move_to_index, only: [:edit, :update]
+  before_action :find_item, only: [:show, :edit, :update]
 
   def index
     @items = Item.order('created_at DESC')
@@ -20,16 +21,12 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
   end
 
   def edit
-    @item = Item.find(params[:id])
   end
 
   def update
-    @item = Item.find(params[:id])
-
     if @item.update(item_params)
       redirect_to item_path(@item.id)
     else
@@ -48,4 +45,9 @@ class ItemsController < ApplicationController
     @item = Item.find(params[:id])
     redirect_to action: :index unless @item.user.id == current_user.id
   end
+
+  def find_item
+    @item = Item.find(params[:id])
+  end
+
 end
